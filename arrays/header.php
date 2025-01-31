2c2c59de5361398137bc65c3c2f36c46977d487a
Yanpemn <victorcestafran08@gmail.com>
2025-01-31 14:56:51 -0300

    dsds

---
+++ arrays/header.php
@@ -0,0 +1,12 @@
+<header class="header">
+
+    <div class="links">
+        <ul>
+            <li>
+                <a href="../index.php">HOME</a>
+                <a href="./pages/portfolio.php">PORTFÓLIO</a>
+            </li>
+        </ul>
+    </div>
+
+</header>
